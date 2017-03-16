099d404f5b7a7690445017a86593b6f98146594a
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-03-16 17:28:02 +0100

    Receive data, debug for game activity

--- app/src/main/java/com/zhenik15/android/tictactoe/GameActivity.java
+++ app/src/main/java/com/zhenik15/android/tictactoe/GameActivity.java
@@ -2,16 +2,17 @@ package com.zhenik15.android.tictactoe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
-/**
- * Created by NIK on 16/03/2017.
- */
 
 public class GameActivity extends AppCompatActivity {
 
     public static final String TAG = "GameActivityTag";
     private String playerName1;
     private String playerName2;
+    private TextView player1;
+    private TextView player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +25,12 @@ public class GameActivity extends AppCompatActivity {
     private void initPlayerNames(){
         playerName1 = getIntent().getStringExtra("userName1");
         playerName2 = getIntent().getStringExtra("userName2");
+        Log.i(TAG, " names:"+playerName1);
+        Log.i(TAG, " names:"+playerName2);
+        player1 = (TextView)findViewById(R.id.game_player1_name);
+        player2 = (TextView)findViewById(R.id.game_player2_name);
+        player1.setText(playerName1);
+        player2.setText(playerName2);
     }
+
 }
