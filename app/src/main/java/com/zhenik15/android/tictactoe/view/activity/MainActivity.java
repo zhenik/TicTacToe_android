2802b679c9c2c82737a2192b798bd340df7fb247
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-03-18 14:51:32 +0100

    Refactoring -> after extracting

--- app/src/main/java/com/zhenik15/android/tictactoe/view/activity/MainActivity.java
+++ app/src/main/java/com/zhenik15/android/tictactoe/view/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.zhenik15.android.tictactoe;
+package com.zhenik15.android.tictactoe.view.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.zhenik15.android.tictactoe.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
