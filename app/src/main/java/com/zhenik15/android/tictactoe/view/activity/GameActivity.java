dd59c5645149c139db6837040f2fa6b0c2e39961
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-03-18 16:11:17 +0100

    Refactor GameActivity -> extract methods, make order in code

--- app/src/main/java/com/zhenik15/android/tictactoe/view/activity/GameActivity.java
+++ app/src/main/java/com/zhenik15/android/tictactoe/view/activity/GameActivity.java
@@ -10,59 +10,51 @@ import android.widget.TextView;
 import com.zhenik15.android.tictactoe.R;
 import com.zhenik15.android.tictactoe.controller.GameController;
 import com.zhenik15.android.tictactoe.models.Board;
+import com.zhenik15.android.tictactoe.models.Player;
 
 
 public class GameActivity extends AppCompatActivity{
 
-    public static final String TAG = "GameActivityTag";
+    public static final String TAG = "GameActivity:> ";
 
     private GameController gameController;
     private Board gameBoard;
-    private String playerName1;
-    private String playerName2;
 
-    private TextView player1;
-    private TextView player2;
+    private Player player1;
+    private Player player2;
 
-//    private char[][] gameBoard;
-//    private int turnCounter;
+    private TextView player1NameView;
+    private TextView player2NameView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
 
-
-        initPlayerNames();
+        initPlayers();
         initGameBoard();
+        initInfo();
     }
 
-    private void initPlayerNames(){
-        playerName1 = getIntent().getStringExtra("userName1");
-        playerName2 = getIntent().getStringExtra("userName2");
-        Log.i(TAG, " names:"+playerName1);
-        Log.i(TAG, " names:"+playerName2);
-        player1 = (TextView)findViewById(R.id.game_player1_name);
-        player2 = (TextView)findViewById(R.id.game_player2_name);
-        player1.setText(playerName1);
-        player2.setText(playerName2);
+    private void initPlayers(){
+        player1 = (Player) getIntent().getSerializableExtra("player1");
+        player2 = (Player) getIntent().getSerializableExtra("player2");
     }
 
-    protected void initGameBoard() {
-        gameBoard = new Board();
-        gameController = new GameController(gameBoard);
-        Log.i(TAG, ": after init "+gameBoard.toString());
-        gameBoard.resetGameBoard();
-        Log.i(TAG, ": after reset "+ gameBoard);
+    private void initInfo(){
+        player1NameView = (TextView)findViewById(R.id.game_player1_name);
+        player2NameView = (TextView)findViewById(R.id.game_player2_name);
+        player1NameView.setText(player1.getName());
+        player2NameView.setText(player2.getName());
+    }
+
+    private void initGameBoard() {
         TableLayout table = (TableLayout)findViewById(R.id.game_board);
 
-        for(int i = 0; i<table.getChildCount(); i++){
-            TableRow row = (TableRow) table.getChildAt(i);
-            for(int j = 0; j<row.getChildCount(); j++){
-                TextView cell = (TextView) row.getChildAt(j);
-                cell.setOnClickListener(gameController.getGameClickListener(i, j, cell));
-                Log.i(TAG, " : set up listener for "+cell.getText()+i+j);
-            }
-        }
+        gameController = new GameController(new Board());
+        gameController.resetGameBoard();
+        gameController.setCellListeners(table);
     }
+
 }
