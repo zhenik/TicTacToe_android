b7a10aab70f4b60616b4d992cab291da5bcb0a75
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-03-19 20:26:53 +0100

    Add game symbols class

---
+++ app/src/main/java/com/zhenik15/android/tictactoe/model/GameSymbol.java
@@ -0,0 +1,7 @@
+package com.zhenik15.android.tictactoe.model;
+
+public abstract class GameSymbol {
+    public static final char X = 'X';
+    public static final char O = 'O';
+    public static final char EMPTY = ' ';
+}
