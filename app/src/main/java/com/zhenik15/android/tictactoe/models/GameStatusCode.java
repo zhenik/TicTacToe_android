5ca616b4a11573b3205aa727ebb65dbd206b3783
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-03-19 16:20:42 +0100

    Update game status codes

--- app/src/main/java/com/zhenik15/android/tictactoe/models/GameStatusCode.java
+++ app/src/main/java/com/zhenik15/android/tictactoe/models/GameStatusCode.java
@@ -2,11 +2,7 @@ package com.zhenik15.android.tictactoe.models;
 
 import java.io.Serializable;
 
-/**
- * Created by NIK on 18/03/2017.
- */
-
-public class GameStatusCode implements Serializable {
+public abstract class GameStatusCode implements Serializable {
     public static final int DRAW = -1;
     public static final int CONTINUE = 0;
     public static final int X_WIN = 1;
