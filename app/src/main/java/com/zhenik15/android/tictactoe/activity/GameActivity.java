524a8ac0e149e8038c8f97bbea628d96f0c15075
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-03-19 20:27:31 +0100

    Update Game activity

--- app/src/main/java/com/zhenik15/android/tictactoe/activity/GameActivity.java
+++ app/src/main/java/com/zhenik15/android/tictactoe/activity/GameActivity.java
@@ -12,10 +12,11 @@ import android.widget.Toast;
 import com.zhenik15.android.tictactoe.R;
 import com.zhenik15.android.tictactoe.model.Board;
 import com.zhenik15.android.tictactoe.model.GameStatusCode;
+import com.zhenik15.android.tictactoe.model.GameSymbol;
 import com.zhenik15.android.tictactoe.model.Player;
 
 
-public class GameActivity extends AppCompatActivity{
+public class GameActivity extends AppCompatActivity {
 
     public static final String TAG = "GameActivity:> ";
 
@@ -30,11 +31,7 @@ public class GameActivity extends AppCompatActivity{
     private Player player2;
 
     // Game
-    private final char X = 'X';
-    private final char O = 'O';
-    private final char EMPTY = ' ';
     private int turnCounter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,101 +42,83 @@ public class GameActivity extends AppCompatActivity{
         initBoard();
         initNavigation();
         navigation.setVisibility(View.INVISIBLE);
+        resetGame();
     }
 
-    private void initPlayers(){
+    /**
+     * SET & INIT  methods
+     */
+
+    private void initPlayers() {
         player1 = (Player) getIntent().getSerializableExtra("player1");
         player2 = (Player) getIntent().getSerializableExtra("player2");
 
-        ((TextView)findViewById(R.id.game_player1_name)).setText(player1.getName());
-        ((TextView)findViewById(R.id.game_player2_name)).setText(player2.getName());
+        ((TextView) findViewById(R.id.game_player1_name)).setText(player1.getName());
+        ((TextView) findViewById(R.id.game_player2_name)).setText(player2.getName());
 
-        scorePlayer1=(TextView)findViewById(R.id.game_player1_score);
-        scorePlayer2=(TextView)findViewById(R.id.game_player2_score);
+        scorePlayer1 = (TextView) findViewById(R.id.game_player1_score);
+        scorePlayer2 = (TextView) findViewById(R.id.game_player2_score);
         updateScore();
 
     }
 
-    private void setScore(TextView scoreView, Player player){
-        scoreView.setText("Score: "+player.getScore());
+    private void setScore(TextView scoreView, Player player) {
+        scoreView.setText("Score: " + player.getScore());
     }
 
-    private void updateScore(){
+    private void updateScore() {
         setScore(scorePlayer1, player1);
         setScore(scorePlayer2, player2);
     }
 
-    private void initNavigation(){
-        navigation= findViewById(R.id.game_nav_panel);
-    }
-    private void initBoard(){
-        TableLayout table = (TableLayout)findViewById(R.id.game_board);
-        gameBoard=new Board(table);
-        resetGameBoard();
-        setCellListeners();
+    private void initNavigation() {
+        navigation = findViewById(R.id.game_nav_panel);
     }
 
-
-    public void resetGameBoard(){
-        turnCounter=0;
-        for(int i = 0; i<3; i++){
-            for(int j = 0; j<3; j++){
-                gameBoard.getTable()[i][j] = EMPTY;
-                Log.i(TAG, "|"+ gameBoard.getTable()[i][j]+"|");
-            }
-        }
+    private void initBoard() {
+        TableLayout table = (TableLayout) findViewById(R.id.game_board);
+        gameBoard = new Board(table);
     }
 
-    private boolean isCellAvailable(int i, int j){
-        return gameBoard.getTable()[i][j]==EMPTY;
-    }
-
-    private char nextTurn(){
-        turnCounter++;
-        if (turnCounter%2==0)return O;
-        return X;
-    }
-
-
-    public void setCellListeners(){
+    public void setCellListeners() {
         TableLayout table = gameBoard.getTableLayout();
-        for(int i = 0; i<table.getChildCount(); i++){
+        for (int i = 0; i < table.getChildCount(); i++) {
             TableRow row = (TableRow) table.getChildAt(i);
-            for(int j = 0; j<row.getChildCount(); j++){
+            for (int j = 0; j < row.getChildCount(); j++) {
                 TextView cell = (TextView) row.getChildAt(j);
                 cell.setOnClickListener(getGameClickListener(i, j, cell));
-                Log.i(TAG, "set up listener for "+cell.getText()+i+j);
+                Log.i(TAG, "set up listener for " + cell.getText() + i + j);
             }
         }
     }
-    private void unsetCellListeners(){
-        for(int i = 0; i<gameBoard.getTableLayout().getChildCount(); i++){
+
+    private void unsetCellListeners() {
+        for (int i = 0; i < gameBoard.getTableLayout().getChildCount(); i++) {
             TableRow row = (TableRow) gameBoard.getTableLayout().getChildAt(i);
-            for(int j = 0; j<row.getChildCount(); j++){
+            for (int j = 0; j < row.getChildCount(); j++) {
                 TextView cell = (TextView) row.getChildAt(j);
                 cell.setOnClickListener(null);
-                Log.i(TAG, "unset listener for "+cell.getText()+i+j);
+                Log.i(TAG, "unset listener for " + cell.getText() + i + j);
             }
         }
     }
 
     //http://stackoverflow.com/questions/10614696/how-to-pass-parameters-to-onclicklistener/10614751
-    public View.OnClickListener getGameClickListener(final int i, final int j, final TextView cell){
-        return new View.OnClickListener(){
+    public View.OnClickListener getGameClickListener(final int i, final int j, final TextView cell) {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                char turnSymbol=nextTurn();
+                char turnSymbol = nextTurn();
 
-                if(isCellAvailable(i,j)) {
-//                    char turnSymbol=nextTurn();
+                if (gameBoard.isCellAvailable(i, j)) {
 //                    Log.i(TAG, "|turn=|"+turnSymbol+"|");
-                    gameBoard.getTable()[i][j]=turnSymbol;
+                    gameBoard.getTable()[i][j] = turnSymbol;
 //                    Log.i(TAG,": click cell "+ i+j + " : "+gameBoard[i][j]);
                     cell.setText(String.valueOf(turnSymbol));
                 }
 
-                switch (checkGameState(turnSymbol)){
+                switch (checkGameState(turnSymbol)) {
                     case GameStatusCode.DRAW:
                         Toast.makeText(getApplicationContext(), "DRAW", Toast.LENGTH_SHORT).show();
                         stopGame(GameStatusCode.DRAW);
@@ -157,7 +136,22 @@ public class GameActivity extends AppCompatActivity{
         };
     }
 
+    /**
+     * GAME LOGIC
+     */
 
+    public void resetGame() {
+        turnCounter = 0;
+        gameBoard.resetGameBoard();
+        setCellListeners();
+    }
+
+
+    private char nextTurn() {
+        turnCounter++;
+        if (turnCounter % 2 == 0) return GameSymbol.O;
+        return GameSymbol.X;
+    }
 
     private int checkGameState(char turn) {
 
@@ -174,7 +168,7 @@ public class GameActivity extends AppCompatActivity{
             return getWinner(turn);
 
         // DRAW
-        if (turnCounter==9)
+        if (turnCounter == 9)
             return GameStatusCode.DRAW;
 
         // CONTINUE
@@ -182,55 +176,55 @@ public class GameActivity extends AppCompatActivity{
     }
 
 
-    private int getWinner(char turn){
-        if (turn==X)return GameStatusCode.X_WIN;
+    private int getWinner(char turn) {
+        if (turn == GameSymbol.X) return GameStatusCode.X_WIN;
         else return GameStatusCode.O_WIN;
     }
 
-    private boolean checkRows(char turn){
-        for (int j = 0; j<3; j++){
-            int symbolCounter=0;
-            for(int i = 0; i<3; i++){
-                if(gameBoard.getTable()[j][i]==turn)
+    private boolean checkRows(char turn) {
+        for (int j = 0; j < 3; j++) {
+            int symbolCounter = 0;
+            for (int i = 0; i < 3; i++) {
+                if (gameBoard.getTable()[j][i] == turn)
                     symbolCounter++;
             }
-            if(symbolCounter==3)
+            if (symbolCounter == 3)
                 return true;
         }
         return false;
     }
 
-    private boolean checkColumns(char turn){
-        for (int j = 0; j<3; j++){
-            int symbolCounter=0;
-            for(int i = 0; i<3; i++){
-                if(gameBoard.getTable()[i][j]==turn)
+    private boolean checkColumns(char turn) {
+        for (int j = 0; j < 3; j++) {
+            int symbolCounter = 0;
+            for (int i = 0; i < 3; i++) {
+                if (gameBoard.getTable()[i][j] == turn)
                     symbolCounter++;
             }
-            if(symbolCounter==3)
+            if (symbolCounter == 3)
                 return true;
         }
         return false;
     }
 
-    private boolean checkDiagonals(char turn){
+    private boolean checkDiagonals(char turn) {
         int count1 = 0;
         int count2 = 0;
-        for(int i = 0; i<3; i++) {
-            if(gameBoard.getTable()[i][i]==turn)
+        for (int i = 0; i < 3; i++) {
+            if (gameBoard.getTable()[i][i] == turn)
                 count1++;
         }
 
-        for(int i = 0; i<3; i++){
-            if(gameBoard.getTable()[i][3-1-i]==turn)
+        for (int i = 0; i < 3; i++) {
+            if (gameBoard.getTable()[i][3 - 1 - i] == turn)
                 count2++;
         }
-        return (count1==3||count2==3);
+        return (count1 == 3 || count2 == 3);
     }
 
-    private void stopGame(int statusCode){
+    private void stopGame(int statusCode) {
         unsetCellListeners();
-        switch (statusCode){
+        switch (statusCode) {
             case GameStatusCode.DRAW:
                 navigation.setVisibility(View.VISIBLE);
                 break;
