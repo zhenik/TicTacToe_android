521d8f12df981c0837809deb6715585614d7e55a
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-03-20 23:32:49 +0100

    Update Player model -> implement comparable for sorting

--- app/src/main/java/com/zhenik15/android/tictactoe/model/Player.java
+++ app/src/main/java/com/zhenik15/android/tictactoe/model/Player.java
@@ -1,8 +1,10 @@
 package com.zhenik15.android.tictactoe.model;
 
+import android.support.annotation.NonNull;
+
 import java.io.Serializable;
 
-public class Player implements Serializable {
+public class Player implements Serializable, Comparable<Player> {
 
     public static final String TAG = "Player:> ";
 
@@ -12,6 +14,10 @@ public class Player implements Serializable {
     public Player(String name) {
         this.name = name;
         score = 0;
+    }
+    public Player(String name, int score){
+        this.name=name;
+        this.score=score;
     }
 
     public String getName() {
@@ -24,5 +30,12 @@ public class Player implements Serializable {
 
     public int win() {
         return ++score;
+    }
+
+    // For DESC Sort. From biggest to lowest
+    @Override
+    public int compareTo(@NonNull Player o) {
+
+        return -Integer.compare(this.score, o.score);
     }
 }
