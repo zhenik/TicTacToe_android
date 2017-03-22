a6e1064127664bc0bd2a3631b68c19a0647208cf
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-03-22 16:51:22 +0100

    Update GameSymbol -> add comments

--- app/src/main/java/com/zhenik15/android/tictactoe/model/util/GameSymbol.java
+++ app/src/main/java/com/zhenik15/android/tictactoe/model/util/GameSymbol.java
@@ -1,5 +1,8 @@
 package com.zhenik15.android.tictactoe.model.util;
-
+/**
+ * Abstract class
+ * defined Game symbols
+ * */
 public abstract class GameSymbol {
     public static final char X = 'X';
     public static final char O = 'O';
